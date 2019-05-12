4f9b35fd4e769be3cd6071252a93076705abbb7f
JC <thompsonrell@gmail.com>
2019-05-11 22:16:36 -0400

    working app

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+var friends = [{
+    "name": "Gardian of the galaxy",
+    "photo": "https://i.dlpng.com/static/png/334834_thumb.png",
+    "scores": [5, 4, 2, 5, 1, 1, 2, 1, 3, 5]
+}, {
+    "name": "Loki",
+    "photo": "https://i.dlpng.com/static/png/325499_thumb.png",
+    "scores": [3, 4, 2, 4, 4, 1, 1, 2, 3, 4]
+}, {
+    "name": "Hulk",
+    "photo": "https://i.dlpng.com/static/png/304983_thumb.png",
+    "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+}, {
+    "name": "Super Girl",
+    "photo": "https://dlpng.com/png/305046",
+    "scores": [5, 2, 4, 3, 3, 2, 1, 3, 3, 4]
+}, {
+    "name": "Captain America",
+    "photo": "https://i.dlpng.com/static/png/298904_thumb.png",
+    "scores": [4, 1, 5, 3, 4, 5, 2, 3, 4, 5]
+}, {
+    "name": "Hawkeye",
+    "photo": "https://i.dlpng.com/static/png/296166_thumb.png",
+    "scores": [4, 1, 5, 3, 4, 5, 2, 3, 4, 5]
+}, {
+    "name": "Iron Man",
+    "photo": "https://i.dlpng.com/static/png/289436_thumb.png",
+    "scores": [3, 4, 2, 4, 4, 1, 1, 2, 3, 4]
+}, {
+    "name": "Superman",
+    "photo": "https://i.dlpng.com/static/png/284049_thumb.png",
+    "scores": [5, 4, 2, 5, 1, 1, 2, 1, 3, 5]
+}, {
+    "name": "Harley Quinn",
+    "photo": "https://i.dlpng.com/static/png/245798_thumb.png",
+    "scores": [1, 5, 1, 3, 5, 1, 4, 3, 2, 1]
+}, {
+    "name": "Flash",
+    "photo": "https://i.dlpng.com/static/png/244178_thumb.png",
+    "scores": [1, 3, 3, 1, 2, 4, 4, 1, 3, 1]
+}, {
+    "name": "Mystique",
+    "photo": "https://i.dlpng.com/static/png/240609_thumb.png",
+    "scores": [5, 2, 4, 3, 3, 2, 1, 3, 3, 4]
+}, {
+    "name": "Spiderman",
+    "photo": "https://i.dlpng.com/static/png/232094_thumb.png",
+    "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+}, {
+    "name": "Wonderwoman",
+    "photo": "https://i.dlpng.com/static/png/227964_thumb.png",
+    "scores": [2, 3, 5, 5, 3, 5, 2, 1, 3, 2]
+}, {
+    "name": "Storm",
+    "photo": "https://i.dlpng.com/static/png/220671_thumb.png",
+    "scores": [2, 3, 5, 5, 3, 5, 2, 1, 3, 2]
+}];
+
+module.exports = friends;
---
+++ app/routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require("path");
+
+// Exports the variable
+module.exports = function (app) {
+	// Sets the route for the home page
+	app.get('/', function (req, res) {
+		res.sendFile(path.join(__dirname, '../public/home.html'));
+	});
+
+	// Sets the route for the survey page
+	app.get('/survey', function (req, res) {
+		res.sendFile(path.join(__dirname, '../public/survey.html'));
+	});
+}
